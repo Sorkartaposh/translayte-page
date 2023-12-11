69f4f4eb48340c3d0539770c5987dcc4ef548173
Taposh Sorkar <99810718+Sorkartaposh@users.noreply.github.com>
2023-12-11 20:16:17 +0600

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+(function($){
+	$(document).ready(function() {	
+
+		// Scroll to Top
+		jQuery('.scrolltotop').click(function(){
+			jQuery('html').animate({'scrollTop' : '0px'}, 400);
+			return false;
+		});
+		
+		jQuery(window).scroll(function(){
+			var upto = jQuery(window).scrollTop();
+			if(upto > 500) {
+				jQuery('.scrolltotop').fadeIn();
+			} else {
+				jQuery('.scrolltotop').fadeOut();
+			}
+		});
+
+		// menu close button
+		$(".openbtn").click(function(){
+			$(".btncross").addClass("btnrotate");
+		});
+		$(".btncross").click(function(){
+			$(".btncross").removeClass("btnrotate");
+		});
+		
+
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	});
+})(jQuery);
